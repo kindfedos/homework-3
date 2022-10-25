d86d42f2c512eba5d12f17f438a34037ef713685
Fedor <kurnosenkovf@mail.ru>
2022-10-25 17:08:48 +0400

    23

---
+++ 23/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите любое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= num; i++) Console.Write($"{Math.Pow(i, 3)}, ");
+Console.WriteLine(" ");
+
+return;
+            
