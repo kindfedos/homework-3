122a7b6bc3c44c8e2503f35d8d2210ddebb91e4a
Fedor <kurnosenkovf@mail.ru>
2022-10-25 16:55:21 +0400

    19

---
+++ 19/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.Clear();
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if (len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - Не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
+}
+
